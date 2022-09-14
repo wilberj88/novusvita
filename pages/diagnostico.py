9591eea2915cee701014babb11f1850c125521db
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-14 12:52:39 -0500

    Update diagnostico.py

--- pages/diagnostico.py
+++ pages/diagnostico.py
@@ -4,4 +4,6 @@ import streamlit as st
 st.title('Define tu esencia')
 
 st.selectbox('Elige el rol más demandado a futuro que desees abordar', ['Data Scientist', 'Broker', 'ML Operator'])
-st.multiselect('Elige los problemas del planeta que deseas enfrentar', ['Hambre', 'Pobreza', 'Educacion'])
+st.multiselect('Selecciona los problemas del planeta que deseas enfrentar', ['Hambre', 'Pobreza', 'Educacion'])
+st.multiselect('Selecciona tus principales pasatiempos', ['Leer', 'Ejercicio', 'Cine'])
+st.text_input('Indícanos actualmente en qué te ganas la vida')
