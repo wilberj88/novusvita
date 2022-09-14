22b69d27e20420a7774094d0de97ad12fab7b5da
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-14 12:40:10 -0500

    Create diagnostico.py

---
+++ pages/diagnostico.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.title('Define tu esencia')
+
+st.radio('Pick one', ['cats', 'dogs'])
+st.selectbox('Pick one', ['cats', 'dogs'])
+st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
