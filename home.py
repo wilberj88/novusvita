1dcdcecdc887f5a81783ae47ec59fc90fab42f5e
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-14 12:20:19 -0500

    Create home.py

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Vita", page_icon="♥")
+
+st.title('Novus Vita ♥')
+st.header("Construye una Nueva Vida. Enfócate solo en lo esencial 🎯")
+
+st.write("Hola 👋 Diligencia el formulario y crearemos un Mando exclusivo para tomes el control de tu vida")
